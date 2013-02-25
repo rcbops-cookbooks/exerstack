221b1ef6f88649a669da3180169b6567225d207a
Darren Birkett <darren.birkett@gmail.com>
2013-02-25 11:25:16 -0600

    tidy up templating

--- recipes/default.rb
+++ recipes/default.rb
@@ -42,46 +42,15 @@ execute "git clone https://github.com/rcbops/exerstack" do
   not_if do File.exists?("/opt/exerstack") end
 end
 
-if Chef::Config[:solo]
-  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
-else
-  keystone = search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
-  if keystone.length > 0
-    keystone_admin_username = keystone[0]['keystone']['admin_user']
-    keystone_admin_password = keystone[0]['keystone']['users'][keystone_admin_username]['password']
-    keystone_admin_tenantname = keystone[0]['keystone']['users'][keystone_admin_username]['default_tenant']
-    keystone_internal_url = keystone[0]['keystone']['internalURL']
-    keystone_admin_url = keystone[0]['keystone']['adminURL']
-    keystone_admin_token = keystone[0]['keystone']['admin_token']
+keystone = get_settings_by_role("keystone", "keystone")
+ks_service_endpoint = get_access_endpoint("keystone-api", "keystone","service-api")
 
-    # determine if we have a nova-api-ec2 endpoint - if we do not then set the variables to a dummy
-    # value so the template will process correctly.
-    if keystone[0]["credentials"].nil?
-      Chef::Log.warn("No EC2 credentials have been created.  Setting EC2 credentials to undefined")
-      ec2_access = "undefined"
-      ec2_secret = "undefined"
-    else
-      ec2_access = keystone[0]["credentials"]["EC2"]["admin"]["access"]
-      ec2_secret = keystone[0]["credentials"]["EC2"]["admin"]["secret"]
-    end
-  else
-    keystone_admin_username = node['keystone']['admin_user']
-    keystone_admin_password = node['keystone']['users'][keystone_admin_username]['password']
-    keystone_admin_tenantname = node['keystone']['users'][keystone_admin_username]['default_tenant']
-    keystone_internal_url = node['keystone']['internalURL']
-    keystone_admin_url = node['keystone']['adminURL']
-    keystone_admin_token = node['keystone']['admin_token']
-    ec2_access = node["credentials"]["EC2"]["admin"]["access"]
-    ec2_secret = node["credentials"]["EC2"]["admin"]["secret"]
-  end
-end
+keystone_admin_username = keystone['admin_user']
+keystone_admin_password = keystone['users'][keystone_admin_username]['password']
+keystone_admin_tenantname = keystone['users'][keystone_admin_username]['default_tenant']
+keystone_service_url = ks_service_endpoint['uri']
 
-ec2_public_endpoint = get_bind_endpoint("nova", "ec2-public")
-# ghetto up a value in ec2_public_endpoint if one doesn't exist
-# you know - so we can run exerstack without an ec2 endpoint
-if ec2_public_endpoint.nil?
-  ec2_public_endpoint={"uri"=>"undefined"}
-end
+ec2_public_endpoint = get_access_endpoint("nova-api-ec2", "nova", "ec2-public")["uri"]
 
 # This is ghetto.. but i am trying to get nova allinone working
 swift = get_settings_by_role("swift-proxy-server", "swift")
@@ -96,19 +65,23 @@ template "/opt/exerstack/localrc" do
   owner "root"
   group "root"
   mode "0644"
-  variables(
+  vars = {
     "keystone_admin_username" => keystone_admin_username,
     "keystone_admin_password" => keystone_admin_password,
     "keystone_admin_tenantname" => keystone_admin_tenantname,
-    "keystone_internal_url" => keystone_internal_url,
-    "keystone_admin_url" => keystone_admin_url,
+    "keystone_service_url" => keystone_service_url,
     "keystone_region_name" => "RegionOne",
-    "keystone_admin_token" => keystone_admin_token,
-    "ec2_url" => ec2_public_endpoint["uri"],
-    "ec2_access" => ec2_access,
-    "ec2_secret" => ec2_secret,
+    "keystone_admin_password" => keystone_admin_password,
     "swift_authtype" => swift_authmode
-  )
+  }
+  if ec2_public_endpoint
+    vars["ec2_url"] = ec2_public_endpoint
+  end
+  unless keystone["credentials"].nil?
+    vars["ec2_access"] = keystone["credentials"]["EC2"]["admin"]["access"]
+    vars["ec2_secret"] = keystone["credentials"]["EC2"]["admin"]["secret"]
+  end
+  variables(vars)
 end
 
 # execute "run exerstack" do
